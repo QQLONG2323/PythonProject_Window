ae9a2ca389d44a9a76d501b889cf12f41ebd787d
余友中 <qqlong2323@gmail.com>
2023-03-31 14:34:05 +0800

    modify

--- drawing/index.py
+++ drawing/index.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 class Window(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        drawingFrame = ttk.Labelframe(self, text = '這裡是畫圖區')
-        drawingFrame.pack(padx=50, pady=50)
-        tk.Button(drawingFrame, text = "Press Me", padx=10, pady=10).pack(padx=30, pady=20)
+
 
 
 
